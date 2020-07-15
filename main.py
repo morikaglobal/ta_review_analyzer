d8c68e7565e16cb47463c8f44e2d064f92657035
morika <sm+github@morikaglobal.com>
2020-07-15 12:11:25 +0900

    heroku webdriver test3

--- main.py
+++ main.py
@@ -3,35 +3,16 @@ from flask import Flask
 import os
 from selenium import webdriver
 
-# chrome driverの配置場所を設定
-CHROME_DRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-# ダウンロードフォルダ―の場所を設定・作成
-DOWNLOAD_DIR = '/app/tmp'
-os.mkdir(DOWNLOAD_DIR)
-print(DOWNLOAD_DIR)
+chrome_options.add_argument('--headless')
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-# chromeの起動オプションを設定
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--window-size=1920,1080')
-options.add_argument('start-maximized')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+URL = 'https://google.com/'
 
-# chromeの追加設定(driverの場所やファイル等のダウンロード先)
-prefs = {'download.default_directory': DOWNLOAD_DIR,
-         'download.prompt_for_download': False}
-options.add_experimental_option('prefs', prefs)
-browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH,
-                           options=options)
-browser.command_executor._commands['send_command'] = ('POST',
-                                                      '/session/$sessionId/chromium/send_command')
-params = {'cmd': 'Page.setDownloadBehavior',
-          'params': {'behavior': 'allow',
-                     'downloadPath': DOWNLOAD_DIR}}
-browser.execute('send_command', params)
 
 
 
@@ -41,13 +22,12 @@ app = Flask(__name__)
 def index():
 
     try:
-        URL = 'https://google.com/'
-        browser.get(URL)
+        driver.get(URL)
         return("hohoho success" + URL)
     except:
         return ("Error Error")
     finally:
-        browser.close()
+        driver.close()
 
 
 
