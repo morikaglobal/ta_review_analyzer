2ccd4aebb496b0ce20abe534f76eca94d188e9e3
morika <sm+github@morikaglobal.com>
2020-07-17 19:18:05 +0900

    fixing error6

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request
 import requests
-# from forms import UrlSearchForm
+from forms import UrlSearchForm
 
-# import time
-# import os
-# from selenium import webdriver
-# from selenium.common.exceptions import NoSuchElementException
+import time
+import os
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument('--no-sandbox')
-# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+chrome_options.add_argument('--headless')
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-# URL = 'https://google.com/'
+URL = 'https://google.com/'
 
 app = Flask(__name__)
 
