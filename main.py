cf3a28764f3f2fe67706daae3bd0e6058e9009fd
morika <sm+github@morikaglobal.com>
2020-07-20 16:15:31 +0900

    testing7

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 chrome_options.add_argument('--headless')
-chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--no-sandbox')
 #error on heroku happens from codes below
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
 # driver.get("https://google.com/")
 
 # URL = 'https://google.com/'
