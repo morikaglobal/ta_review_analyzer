55465e75d6b650836502cf5c18c969526ee8e499
morika <sm+github@morikaglobal.com>
2020-07-20 16:28:41 +0900

    testing9

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ from selenium import webdriver
 import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+
+# options = webdriver.ChromeOptions()
+chrome_options.add_argument('--lang=en')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu');
+chrome_options.add_argument('--disable-extensions');
+chrome_options.add_argument('--proxy-server="direct://"');
+chrome_options.add_argument('--proxy-bypass-list=*');
+chrome_options.add_argument('--start-maximized');
+
+
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
 
 app = Flask(__name__)
 
