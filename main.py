de68c80ec84af06a948965a9ab6343ef2d1a54e9
morika <sm+github@morikaglobal.com>
2020-07-20 15:44:26 +0900

    testing2

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from selenium.common.exceptions import NoSuchElementException
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-chrome_options.add_argument('--headless')
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument('--no-sandbox')
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # URL = 'https://google.com/'
 
