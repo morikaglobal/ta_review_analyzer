edf4fb05fd1f402619088536ab130001f0e534bd
morika <sm+github@morikaglobal.com>
2020-07-20 16:19:50 +0900

    testing8

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 from flask import Flask, render_template, request
-import requests
-from forms import UrlSearchForm
 
-import time
-import os
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-
+import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-chrome_options.add_argument('--headless')
-# chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument('--no-sandbox')
-#error on heroku happens from codes below
-driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
-# driver.get("https://google.com/")
-
-# URL = 'https://google.com/'
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 app = Flask(__name__)
 
@@ -30,4 +21,38 @@ def index():
  
 if __name__ == '__main__':
       app.run()
+
+
+# from flask import Flask, render_template, request
+# import requests
+# from forms import UrlSearchForm
+
+# import time
+# import os
+# from selenium import webdriver
+# from selenium.common.exceptions import NoSuchElementException
+
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+# chrome_options.add_argument('--headless')
+# # chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument('--no-sandbox')
+# #error on heroku happens from codes below
+# driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+# # driver.get("https://google.com/")
+
+# # URL = 'https://google.com/'
+
+# app = Flask(__name__)
+
+# # @app.route('/', methods = ["GET", "POST"])
+# @app.route('/')
+
+# def index():
+#     return ("Hello Hello")
+
+ 
+# if __name__ == '__main__':
+#       app.run()
 
