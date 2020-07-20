f79f99c5fe0d3ec1437a2bc2f9d56d61b168a22c
morika <sm+github@morikaglobal.com>
2020-07-20 16:41:30 +0900

    testing10

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ chrome_options.add_argument('--start-maximized');
 # chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+# driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
 
 app = Flask(__name__)
 
