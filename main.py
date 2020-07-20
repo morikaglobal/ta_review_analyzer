9beea363ad22f3ba3ef426b6785f6360510586db
morika <sm+github@morikaglobal.com>
2020-07-20 16:09:20 +0900

    testing6

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--no-sandbox')
 #error on heroku happens from codes below
-
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 # driver.get("https://google.com/")
 
