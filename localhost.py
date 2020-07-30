102f9e7c165e40e5d66a7da537f3cce08a976df6
morika <sm+github@morikaglobal.com>
2020-07-30 18:29:44 +0900

    testing getting location

--- localhost.py
+++ localhost.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template, request
 import requests
 from forms import UrlSearchForm
 
-from selenium import webdriver
+import time
 import os
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--no-sandbox')
-driver= webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+# commented out for heroku deployment
+driver_location = r"C:\Users\masao\Anaconda3\chromedriver.exe"
 
+options = webdriver.ChromeOptions()
+
+options.add_argument('--lang=en')
+options.add_argument('--headless')
+driver = webdriver.Chrome(executable_path=driver_location, options=options)
 
 app = Flask(__name__)
 
-# @app.route('/')
+@app.route('/')
 
 # def index():
 #     return ("Hello")
