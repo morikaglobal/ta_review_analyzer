24abc54dc8e068fdfa4e3c1a8a9dec81f30533c7
morika <sm+github@morikaglobal.com>
2021-06-19 14:11:12 +0900

    nltk working

--- main.py
+++ main.py
@@ -2,10 +2,21 @@
 
 from flask import Flask, render_template, request
 
+
+import nltk
+nltk.data.path.append('TA_REVIEW_ANALYZER/nltk_data')
+sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
+
+tokens = nltk.word_tokenize(sentence)
+print(tokens)
+testing = tokens[1]
+print(testing)
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return testing
     return render_template('index.html')
 
 
