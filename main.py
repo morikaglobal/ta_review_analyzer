515806ad1ebc0b6a4ec5f3cc95d66839bd5bb47c
morika <sm+github@morikaglobal.com>
2021-06-19 17:56:54 +0900

    adding Procfile

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
 
 tokens = nltk.word_tokenize(sentence)
 print(tokens)
-testing = tokens[1]
+testing = tokens[4]
 print(testing)
 
 app = Flask(__name__)
