6e9796ee29bc5521a5a0675fccc55c47715990e8
morika <sm+github@morikaglobal.com>
2021-06-24 11:25:23 +0900

    heroku branch

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 # coding: UTF-8
 
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
+# import nltk
+# nltk.data.path.append('TA_REVIEW_ANALYZER/nltk_data')
+# sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
 
+# tokens = nltk.word_tokenize(sentence)
+# print(tokens)
+# testing = tokens[4]
+# print(testing)
 
-import nltk
-nltk.data.path.append('TA_REVIEW_ANALYZER/nltk_data')
-sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
+import logging
+import os
 
-tokens = nltk.word_tokenize(sentence)
-print(tokens)
-testing = tokens[4]
-print(testing)
+from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+logging.basicConfig(level=logging.INFO)
+logger=logging.getLogger(__name__)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return testing
+    return ("Hello World")
     return render_template('index.html')
 
 
