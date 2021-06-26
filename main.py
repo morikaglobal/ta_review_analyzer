1f35b5adf7d12582ef4dfad2d0dfa2f72cf252fa
morika <sm+github@morikaglobal.com>
2021-06-26 23:01:42 +0900

    adding to working code

--- main.py
+++ main.py
@@ -3,21 +3,33 @@
 # import logging
 # import os
 
-from flask import Flask
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
+from flask import Flask, request
+import requests
+
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 # logging.basicConfig(level=logging.INFO)
 # logger=logging.getLogger(__name__)
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-dev-shm-usage')
-# chrome_options.add_argument('--no-sandbox')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
 
-# driver= webdriver.Chrome(executable_path=r'C:/Users/USER/chromedriver.exe', chrome_options=chrome_options)
+# for LOCAL HOST comment out for deployment
+# driver_path = r'C:/Users/USER/chromedriver.exe'
+
+# for heroku deployment
+driver_path = '/app/.chromedriver/bin/chromedriver'
+
+driver= webdriver.Chrome(executable_path=driver_path, chrome_options=chrome_options)
+
+
 
 
 
@@ -26,10 +38,10 @@ app = Flask(__name__)
 @app.route('/')
 def index(): 
 
-    # url = "http://www.yahoo.co.jp"
-    # driver.get(url)   
+    url = "http://www.yahoo.co.jp"
+    driver.get(url)   
     
-    return ("Hello Hello World mainpy working")
+    return ("Hello Hello getting yahoo")
     
 
 
