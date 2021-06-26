617ef8fbd9b124c865574597351e9d84cad064c9
morika <sm+github@morikaglobal.com>
2021-06-26 11:56:03 +0900

    adding buildpack

--- main.py
+++ main.py
@@ -21,11 +21,23 @@ from selenium.webdriver.common.by import By
 logging.basicConfig(level=logging.INFO)
 logger=logging.getLogger(__name__)
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
+
+driver= webdriver.Chrome(executable_path=r'C:/Users/USER/chromedriver.exe', chrome_options=chrome_options)
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return ("Hello World")
+def index():    
+    
+    url = "http://www.yahoo.co.jp"
+    driver.get(url)
+    return ("Hello Hello World")
     return render_template('index.html')
 
 
